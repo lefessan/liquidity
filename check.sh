3622e51f230459663900597f20644dbbdc665b37
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2017-10-22 18:17:02 +0200

    Fix tests and add bet_report.liq

--- check.sh
+++ check.sh
@@ -7,14 +7,19 @@ test=$1
 echo "\n[check.sh] test = $test"
 
 LIQUIDITY=liquidity
+LIQUIDITY_ENV=ocplib-liquidity-env
+TEZOS=./tezos/_obuild/tezos-client/tezos-client.asm
+LIQUIDITY_COMP=./_obuild/ocp-liquidity-comp/ocp-liquidity-comp.asm
+LIQUIDITY_MISC=ocplib-liquidity-misc
+LIQUIDITY_ENV=ocplib-liquidity-env
 
 echo ./_obuild/${LIQUIDITY}/${LIQUIDITY}.asm tests/$test.liq
 ./_obuild/${LIQUIDITY}/${LIQUIDITY}.asm tests/$test.liq || exit 2
 
-if [ -f ./tezos/_obuild/tezos-client/tezos-client.asm ] ; then
-    ./tezos/_obuild/tezos-client/tezos-client.asm typecheck program tests/$test.liq.tz || exit 2
+if [ -f ${TEZOS} ] ; then
+    ${TEZOS} typecheck program tests/$test.liq.tz || exit 2
 else
-    echo "\n${RED}./tezos/_obuild/tezos-client/tezos-client.asm not present ! typechecking of tests/$test.liq.tz skipped${DEFAULT}\n"
+    echo "\n${RED}${TEZOS} not present ! typechecking of tests/$test.liq.tz skipped${DEFAULT}\n"
 fi
 
 echo ./_obuild/${LIQUIDITY}/${LIQUIDITY}.asm tests/$test.liq.tz
@@ -23,12 +28,12 @@ echo ./_obuild/${LIQUIDITY}/${LIQUIDITY}.asm tests/$test.liq.tz
 echo ./_obuild/${LIQUIDITY}/${LIQUIDITY}.asm tests/$test.liq.tz.liq 
 ./_obuild/${LIQUIDITY}/${LIQUIDITY}.asm tests/$test.liq.tz.liq || exit 2
 
-if [ -f ./tezos/_obuild/tezos-client/tezos-client.asm ] ; then
-    ./tezos/_obuild/tezos-client/tezos-client.asm typecheck program tests/$test.liq.tz.liq.tz || exit 2
+if [ -f ${TEZOS} ] ; then
+    ${TEZOS} typecheck program tests/$test.liq.tz.liq.tz || exit 2
 else
-    echo "\n${RED}./tezos/_obuild/tezos-client/tezos-client.asm not present ! typechecking of tests/$test.liq.tz.liq.tz skipped${DEFAULT}\n"
+    echo "\n${RED}${TEZOS} not present ! typechecking of tests/$test.liq.tz.liq.tz skipped${DEFAULT}\n"
 fi
 
-echo ./_obuild/ocp-liquidity-comp/ocp-liquidity-comp.asm -I +../zarith zarith.cma -I _obuild/liquidity-env ./_obuild/liquidity-env/liquidity-env.cma -impl tests/$test.liq
-./_obuild/ocp-liquidity-comp/ocp-liquidity-comp.asm -I +../zarith zarith.cma -I _obuild/liquidity-env unix.cma ./_obuild/liquidity-env/liquidity-env.cma -dsource -impl tests/$test.liq || exit 2
+echo ${LIQUIDITY_COMP} unix.cma -I +../zarith zarith.cma -I _obuild/${LIQUIDITY_MISC} -I _obuild/${LIQUIDITY_ENV} ./_obuild/${LIQUIDITY_MISC}/${LIQUIDITY_MISC}.cma ./_obuild/${LIQUIDITY_ENV}/${LIQUIDITY_ENV}.cma -impl tests/$test.liq
+ ${LIQUIDITY_COMP} unix.cma -I +../zarith zarith.cma -I _obuild/${LIQUIDITY_MISC} -I _obuild/${LIQUIDITY_ENV} ./_obuild/${LIQUIDITY_MISC}/${LIQUIDITY_MISC}.cma ./_obuild/${LIQUIDITY_ENV}/${LIQUIDITY_ENV}.cma -impl tests/$test.liq || exit 2
 rm -f a.out
